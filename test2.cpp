6ecc6e01988ef4cd3addaf38fd84c9717a7dd48f
oblivion777 <dyangel@foxmail.com>
2022-01-30 01:18:51 +0800

    Update test2.cpp

--- test2.cpp
+++ test2.cpp
@@ -18,11 +18,11 @@ sql::Connection* connect();
 
 locale loc("chs");//windows下ok
 int main(int argc,char *argv[]) {
-    fileList1();
+    //fileList1();
     //listFilesCurrent("E:\\Games\\MOD\\*.*");
     FileMD5Thread sqlTest = FileMD5Thread();
     sql::Connection* conn = connect();
-    //sqlTest.run(conn);
+    sqlTest.run(conn);
     //sqlTest.run();
     conn->close();
     //wprintf(L"Kräuter Body 2.0");
@@ -46,28 +46,29 @@ sql::Connection* connect() {
     std::string password;
     char loginInfoBuff[loginInfoLen] = { 0 };
 
-    loginInfo.open("_log/login_info.txt",ios::in);
+    loginInfo.open("_log/login_info.txt", ios::in);
     while (loginInfo.getline(loginInfoBuff, loginInfoLen))
-    { 
-        if ((strstr(loginInfoBuff, "url:\t"))) {
-            url.append(loginInfoBuff + strlen("url:\t"));
+    {
+        if ((strstr(loginInfoBuff, "url: "))) {
+            url.append(loginInfoBuff + strlen("url: "));
         }
-        else if ((strstr(loginInfoBuff, "username:\t"))) {
-            username.append(loginInfoBuff + strlen("username:\t"));
-        }    
-        else if ((strstr(loginInfoBuff, "password:\t"))) {
-            password.append(loginInfoBuff + strlen("password:\t"));
+        else if ((strstr(loginInfoBuff, "username: "))) {
+            username.append(loginInfoBuff + strlen("username: "));
+        }
+        else if ((strstr(loginInfoBuff, "password: "))) {
+            password.append(loginInfoBuff + strlen("password: "));
         }
     }
     try
-    { 
+    {
         driver = sql::mysql::get_mysql_driver_instance();
         con = driver->connect(url, username, password);
     }
     catch (...)
     {
         cout << "connect failed!" << endl;
-        return NULL;
+        system("pause");
+        exit(1);
     }
     cout << "connect suceess!" << endl;
     return con;
